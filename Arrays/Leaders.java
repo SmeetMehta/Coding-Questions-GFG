075aa483143feec4fc14dc4130ee5733927a50b9
Smeet Mehta <59577058+SmeetMehta@users.noreply.github.com>
2021-05-30 22:39:48 +0530

    Leaders in an array (Java)
    Given an array A of positive integers. Your task is to find the leaders in the array. An element of array is leader if it is greater than or equal to all the elements to its right side. The rightmost element is always a leader. 
    Input:
    n = 6
    A[] = {16,17,4,3,5,2}
    Output: 17 5 2
    Explanation: The first leader is 17 
    as it is greater than all the elements
    to its right.  Similarly, the next 
    leader is 5. The right most element 
    is always a leader so it is also 
    included.
    
    Expected Time Complexity: O(n)
    Expected Auxiliary Space: O(n)

---
+++ Arrays/Leaders.java
@@ -0,0 +1,63 @@
+/ { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Array {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		
+		while(t-->0){
+		    
+		    //input size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //inserting elements in the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    StringBuffer str = new StringBuffer();
+		    ArrayList<Integer> res = new ArrayList<Integer>();
+		  
+		    //calling leaders() function
+		    res = obj.leaders(arr, n);
+		    
+		    //appending result to a String
+		    for(int i=0; i<res.size(); i++){
+		        str.append(res.get(i)+" ");
+		    }
+		    
+		    //printing the String
+		    System.out.println(str);
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int max = arr[n-1];
+        list.add(max);
+        for(int i=n-2; i >=0; i--){
+            if(arr[i] >= max){
+                
+                list.add(arr[i]);
+                max = arr[i];
+            }
+        }
+        Collections.reverse(list);
+        return list;
+        }
+    // Your code here
+}
