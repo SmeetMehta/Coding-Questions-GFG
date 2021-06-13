493c5bc9418d793c0d0956fde3355da3586a4414
Smeet Mehta <59577058+SmeetMehta@users.noreply.github.com>
2021-06-13 22:08:08 +0530

    Longest Palindrome in a String (Python)
    Given a string S, find the longest palindromic substring in S. Substring of string S: S[ i . . . . j ] where 0 ≤ i ≤ j < len(S). Palindrome string: A string which reads the same backwards. More formally, S is palindrome if reverse(S) = S. Incase of conflict, return the substring which occurs first ( with the least starting index).

---
+++ String/longpalin.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution:
+    def longestPalin(self, S):
+        l=""
+        x=""
+        le=0
+        for i in range(0,len(S)):
+            for j in range(i+1,len(S)+1):
+                l=S[i:j]
+                #print(l,len(l))
+                if(l==l[::-1] and len(l)>le):
+                    x=l
+                    le=len(l)  
+        return x
+        # code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        S = input()
+
+        ob = Solution()
+
+        ans = ob.longestPalin(S)
+
+        print(ans)
+# } Driver Code Ends
