89648f2e78ac6550f19972665a640d94464d951e
Smeet Mehta <59577058+SmeetMehta@users.noreply.github.com>
2021-06-25 22:47:11 +0530

    Reverse Linked List in groups of given size (Java)
    Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked list.
    
    Input:
    LinkedList: 1->2->2->4->5->6->7->8
    K = 4
    Output: 4 2 2 1 8 7 6 5 
    Explanation: 
    The first 4 elements 1,2,2,4 are reversed first 
    and then the next 4 elements 5,6,7,8. Hence, the 
    resultant linked list is 4->2->2->1->8->7->6->5.
    
    Expected Time Complexity : O(N)
    Expected Auxilliary Space : O(1)

---
+++ Linked List/reverseByK.java
@@ -0,0 +1,96 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class ReverseInSize
+{
+    static Node head;
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                // addToTheLast(new Node(a));
+                tail.next = new Node(a);
+                tail =tail.next;
+            }
+            
+            int k = sc.nextInt();
+            Solution ob = new Solution();
+            Node res = ob.reverse(head, k);
+            printList(res);
+            System.out.println();
+        }
+    }
+    
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+}
+
+// } Driver Code Ends
+
+
+/*node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution
+{
+    public static Node reverse(Node node, int k)
+    {
+        Node current=node;
+        Node previous=null;
+        Node next=null;
+        int count=0;
+        while(count<k && current!=null){
+            next=current.next;
+            current.next=previous;
+            previous=current;
+            current=next;
+            count++;
+        }
+        if(next!=null){
+            node.next=reverse(next,k);
+        }
+        return previous;
+    }
+}
+
