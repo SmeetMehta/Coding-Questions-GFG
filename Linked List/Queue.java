884d832cb094979ec1e2a59a59f7bce123f26179
Smeet Mehta <59577058+SmeetMehta@users.noreply.github.com>
2021-07-19 14:54:38 +0530

    Implement Queue using Linked List (Java)
    mplement a Queue using Linked List. 
    A Query Q is of 2 Types
    (i) 1 x   (a query of this type means  pushing 'x' into the queue)
    (ii) 2     (a query of this type means to pop an element from the queue and print the poped element)
    
    Input:
    Q = 5
    Queries = 1 2 1 3 2 1 4 2
    Output: 2 3
    Explanation: n the first testcase
    1 2 the queue will be {2}
    1 3 the queue will be {2 3}
    2   poped element will be 2 the
        queue will be {3}
    1 4 the queue will be {3 4}
    2   poped element will be 3.
    
    Expected Time Complexity: O(1).
    Expected Auxiliary Space: O(1).

---
+++ Linked List/Queue.java
@@ -0,0 +1,99 @@
+// { Driver Code Starts
+import java.util.*;
+class QueueNode
+{
+	int data;
+	QueueNode next;
+	QueueNode(int a)
+	{
+	    data = a;
+	    next = null;
+	}
+}
+
+class GfG {
+	public static void main(String args[])
+	{
+		 Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+		 {
+			MyQueue obj = new MyQueue();
+			int Q = sc.nextInt();
+			while(Q>0)
+			{
+				int QueryType = 0;
+				QueryType = sc.nextInt();
+				if(QueryType == 1)
+				{
+					int a = sc.nextInt();
+					
+					obj.push(a);
+					
+				}else if(QueryType == 2)
+				{
+				System.out.print(obj.pop()+" ");
+				}
+				Q--;
+			}
+			System.out.println("");
+			t--;
+		 }
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+/*The structure of the node of the queue is
+class QueueNode
+{
+	int data;
+	QueueNode next;
+	QueueNode(int a)
+	{
+	    data = a;
+	    next = null;
+	}
+}*/
+
+class MyQueue
+{
+    QueueNode front, rear;
+    
+    //Function to push an element into the queue.
+	void push(int a)
+	{
+        // Your code here
+        QueueNode newnode=new QueueNode(a);
+        if(front==null )
+           front = rear = newnode;
+
+        else{
+        rear.next = newnode;
+        rear = newnode;
+        }
+    }
+
+
+    int pop()
+    {
+        if(front == null || rear == null)
+            return -1;
+        else{
+            QueueNode m = front;
+            front = front.next;
+            return m.data;
+            }
+    }
+}
+
+
+
+
+
+
+
