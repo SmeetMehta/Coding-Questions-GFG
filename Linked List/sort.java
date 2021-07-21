491e077fe9d49afc6f3b8bbc1aa4c6153a15f26a
Smeet Mehta <59577058+SmeetMehta@users.noreply.github.com>
2021-07-21 20:25:53 +0530

    Given a linked list of 0s,1s & 2s, sort it. (Java)
    Given a linked list of N nodes where nodes can contain values 0s, 1s, and 2s only. The task is to segregate 0s, 1s, and 2s linked list such that all zeros segregate to head side, 2s at the end of the linked list, and 1s in the mid of 0s and 2s.
    
    Input:
    N = 8
    value[] = {1,2,2,1,2,0,2,2}
    Output: 0 1 1 2 2 2 2 2
    Explanation: All the 0s are segregated
    to the left end of the linked list,
    2s to the right end of the list, and
    1s in between.
    
    Expected Time Complexity: O(N).
    Expected Auxiliary Space: O(1).

---
+++ Linked List/sort.java
@@ -0,0 +1,103 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            while(n-- > 1){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		   
+		      head = new Solution().segregate(head);
+		     printList(head);
+		    System.out.println();
+        }
+    }
+    
+    public static void printList(Node head)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    
+    
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution
+{
+    //Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head)
+    {
+        // add your code here
+        ArrayList<Integer> pq=new ArrayList<Integer>();
+        Node temp=head;
+        while(temp!=null){
+            pq.add(temp.data);
+            temp=temp.next;
+        }
+        Collections.sort(pq);
+        
+        temp=head;
+        int i=0;
+        while(temp!=null){
+            temp.data=pq.get(i);
+            i++;
+            temp=temp.next;
+        }
+        
+        return head;
+    }
+}
+
+
